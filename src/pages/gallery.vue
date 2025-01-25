a7d7513748690006fdc346fd73a1f10e6aa56736
Bianca Drinovan <bia_drinovan@yahoo.com>
2025-01-25 21:41:00 +0200

    initial commit

---
+++ src/pages/gallery.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "gallery"
+}
+</script>
+
+<template>
+  <div>
+    <h1>Gallery</h1>
+    <p>Welcome to the Gallery page!</p>
+  </div>
+</template>
+
+<style scoped lang="sass">
+
+</style>
