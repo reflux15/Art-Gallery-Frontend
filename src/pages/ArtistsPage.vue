057c28d21a2634c20d13f6590073a3849f074b46
Bianca Drinovan <bia_drinovan@yahoo.com>
2025-02-07 21:04:42 +0200

    add artists search bar

--- src/pages/ArtistsPage.vue
+++ src/pages/ArtistsPage.vue
@@ -1,14 +1,21 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col cols="8">
         <h1>Artists</h1>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field
+          v-model="searchBar"
+          prepend-icon="mdi-magnify"
+          label="Search"
+        ></v-text-field>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="12">
         <div class="d-flex flex-wrap">
-          <artist-card  v-for="i in 10" :key="i"/>
+          <artist-card :name="artist.name" v-for="artist in filteredArtists" :key="artist.name" />
         </div>
       </v-col>
     </v-row>
@@ -17,10 +24,43 @@
 
 <script>
 import ArtistCard from "@/components/artist/ArtistCard.vue";
+import _ from "lodash";
 
 export default {
   name: "ArtistsPage",
-  components: {ArtistCard}
+  components: {ArtistCard},
+  data: () => {
+    return {
+      searchBar: "",
+      artists: [
+        {
+          name: "Robin"
+        },
+        {
+          name: "Mark"
+        },
+        {
+          name: "Gother"
+        },
+        {
+          name: "RVan"
+        },
+        {
+          name: "Flavius"
+        },
+        {
+          name: "Damian"
+        },
+      ]
+    }
+  },
+  computed: {
+    filteredArtists() {
+      return this.artists.filter(artist => {
+        return _.startsWith(artist.name.toLowerCase(), this.searchBar.toLowerCase())
+      })
+    }
+  }
 }
 </script>
 
